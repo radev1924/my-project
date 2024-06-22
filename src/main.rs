c1e2d5894332db33cf59563219deba7769a3ef9e
radev1924 <radev1924@abv.bg>
2024-06-22 16:35:56 +0300

    rrefirmish UI colors and inplemented calcs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,30 @@
+use std::fmt::format;
+
 slint::include_modules!();
+
+const TAXPER: f64 = 0.30;
+const OWNER: f64 = 0.20;
+const PROFIT: f64 = 0.05;
+const OPEX: f64 = 0.10;
 
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
 
-    ui.on_request_increase_value({
+    ui.on_divide_income({
         let ui_handle = ui.as_weak();
-        move || {
+        move |string| {
             let ui = ui_handle.unwrap();
-            ui.set_counter(ui.get_counter() + 1);
+            let num: f64 = string.trim().parse().unwrap();
+
+            let tax: f64 = num * TAXPER;
+            let owner: f64 = num * OWNER;
+            let profit: f64 = num * PROFIT;
+            let opex: f64 = num * OPEX;
+
+            let result: String = format!("Taxes:{:2}\nOwner: {:2}\nProfit: {:2}\nOpex: {:2}", tax, owner, profit, opex);
+
+
+            ui.set_results( result.into());
         }
     });
 
